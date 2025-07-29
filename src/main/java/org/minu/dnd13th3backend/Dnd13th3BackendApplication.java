53e65397b898e20738f3c97f79763b12b63bc810
Gdm0714 <ehdals45454@naver.com>
2025-07-29 23:32:11 +0900

    feat: Spring Boot 프로젝트 초기 설정 - Java 17, Spring Boot 3.4.8 - 필요한 의존성 추가

---
+++ src/main/java/org/minu/dnd13th3backend/Dnd13th3BackendApplication.java
@@ -0,0 +1,13 @@
+package org.minu.dnd13th3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd13th3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dnd13th3BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/minu/dnd13th3backend/Dnd13th3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minu.dnd13th3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd13th3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
