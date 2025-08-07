997f79fb1c781bc6ba514b0eda9288fbbed83e9f
znhxxn <ghoon7777@naver.com>
2025-08-07 17:59:32 +0900

    feat: CI/CD 테스트를 위한 H2 설정 및 검증 메시지 추가:

--- src/test/java/org/minu/dnd13th3backend/Dnd13th3BackendApplicationTests.java
+++ src/test/java/org/minu/dnd13th3backend/Dnd13th3BackendApplicationTests.java
@@ -2,8 +2,10 @@ package org.minu.dnd13th3backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Dnd13th3BackendApplicationTests {
 
     @Test
