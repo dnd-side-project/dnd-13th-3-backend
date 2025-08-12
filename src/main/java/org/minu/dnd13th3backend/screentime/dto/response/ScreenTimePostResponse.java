74f667f425634c793bf7e5698962b40d890b07f1
znhxxn <ghoon7777@naver.com>
2025-08-13 00:08:13 +0900

    feat: screentime 등록 api 구현

--- src/main/java/org/minu/dnd13th3backend/screentime/dto/response/ScreenTimePostResponse.java
+++ src/main/java/org/minu/dnd13th3backend/screentime/dto/response/ScreenTimePostResponse.java
@@ -1,4 +1,20 @@
 package org.minu.dnd13th3backend.screentime.dto.response;
 
+import lombok.Getter;
+import org.minu.dnd13th3backend.screentime.entity.ScreenTime;
+
+import java.time.LocalDate;
+
+@Getter
 public class ScreenTimePostResponse {
+
+    private final Long id;
+    private final LocalDate date;
+    private final Integer screentimeMinutes; // screentime_minutes -> screentimeMinutes
+
+    public ScreenTimePostResponse(ScreenTime screenTime) {
+        this.id = screenTime.getId();
+        this.date = screenTime.getDate();
+        this.screentimeMinutes = screenTime.getScreentimeMinutes();
+    }
 }
--- src/main/java/org/minu/dnd13th3backend/screentime/service/ScreenTimeService.java
+++ src/main/java/org/minu/dnd13th3backend/screentime/service/ScreenTimeService.java
@@ -1,4 +1,36 @@
-package org.minu.dnd13th3backend.challenge.service;
+package org.minu.dnd13th3backend.screentime.service;
 
-public class ChallengeService {
+import lombok.RequiredArgsConstructor;
+import org.minu.dnd13th3backend.screentime.dto.request.ScreenTimePostRequest; // 경로 수정
+import org.minu.dnd13th3backend.screentime.entity.ScreenTime;
+import org.minu.dnd13th3backend.screentime.repository.ScreenTimeRepository;
+import org.minu.dnd13th3backend.user.entity.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ScreenTimeService {
+
+    private final ScreenTimeRepository screenTimeRepository;
+
+    @Transactional
+    public ScreenTime registerOrUpdateScreenTime(ScreenTimePostRequest requestDto, User user) {
+        Optional<ScreenTime> optionalScreenTime = screenTimeRepository.findByUserAndDate(user, requestDto.getDate());
+
+        if (optionalScreenTime.isPresent()) {
+            ScreenTime screenTime = optionalScreenTime.get();
+            screenTime.updateScreenTime(requestDto.getScreentimeMinutes());
+            return screenTime;
+        } else {
+            ScreenTime newScreenTime = ScreenTime.builder()
+                    .user(user)
+                    .date(requestDto.getDate())
+                    .screentimeMinutes(requestDto.getScreentimeMinutes())
+                    .build();
+            return screenTimeRepository.save(newScreenTime);
+        }
+    }
 }
