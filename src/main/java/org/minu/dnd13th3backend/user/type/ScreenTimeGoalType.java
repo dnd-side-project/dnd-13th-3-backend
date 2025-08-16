45f7f2a95dd81185c213c7f998a787f2588b886a
znhxxn <ghoon7777@naver.com>
2025-08-16 16:52:45 +0900

    프로필 조회를 위한 메서드 추가

--- src/main/java/org/minu/dnd13th3backend/user/type/ScreenTimeGoalType.java
+++ src/main/java/org/minu/dnd13th3backend/user/type/ScreenTimeGoalType.java
@@ -1,19 +1,23 @@
 package org.minu.dnd13th3backend.user.type;
 
 public enum ScreenTimeGoalType {
-    TWO_HOURS("2HOURS"), 
-    FOUR_HOURS("4HOURS"), 
-    SIX_HOURS("6HOURS"), 
-    EIGHT_HOURS("8HOURS"), 
-    CUSTOM("CUSTOM");
+    TWO_HOURS("2HOURS", 120),
+    FOUR_HOURS("4HOURS", 240),
+    SIX_HOURS("6HOURS", 360),
+    EIGHT_HOURS("8HOURS", 480),
+    CUSTOM("CUSTOM", 0);
     
     private final String value;
+    private final int minutes;
     
-    ScreenTimeGoalType(String value) {
+    ScreenTimeGoalType(String value, int minutes) {
         this.value = value;
+        this.minutes = minutes;
     }
     
     public String getValue() {
         return value;
     }
+
+    public int getMinutes() { return minutes; }
 }
