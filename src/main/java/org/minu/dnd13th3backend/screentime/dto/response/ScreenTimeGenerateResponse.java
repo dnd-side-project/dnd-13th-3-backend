3d741039937cfc02ec6b6b2aee7a8f936b173ec7
znhxxn <ghoon7777@naver.com>
2025-08-16 16:55:27 +0900

    feature: 스크린타임 생성/갱신 api 구현

--- src/main/java/org/minu/dnd13th3backend/screentime/dto/response/ScreenTimeGenerateResponse.java
+++ src/main/java/org/minu/dnd13th3backend/screentime/dto/response/ScreenTimeGenerateResponse.java
@@ -6,13 +6,13 @@ import org.minu.dnd13th3backend.screentime.entity.ScreenTime;
 import java.time.LocalDate;
 
 @Getter
-public class ScreenTimePostResponse {
+public class ScreenTimeGenerateResponse {
 
     private final Long id;
     private final LocalDate date;
-    private final Integer screentimeMinutes; // screentime_minutes -> screentimeMinutes
+    private final Integer screentimeMinutes;
 
-    public ScreenTimePostResponse(ScreenTime screenTime) {
+    public ScreenTimeGenerateResponse(ScreenTime screenTime) {
         this.id = screenTime.getId();
         this.date = screenTime.getDate();
         this.screentimeMinutes = screenTime.getScreentimeMinutes();
