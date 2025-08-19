190ab97516f3a35275d75f31f60a9a4119071cfd
znhxxn <ghoon7777@naver.com>
2025-08-19 23:38:04 +0900

    fix: 스크린타임 생성, 조회 + 챌린지 조회  api 수정 - 4개의 어플(카카오, 크롬, 인스타, 유튜브) 값 반영

--- src/main/java/org/minu/dnd13th3backend/screentime/dto/response/ScreenTimeGetDailyResponse.java
+++ src/main/java/org/minu/dnd13th3backend/screentime/dto/response/ScreenTimeGetDailyResponse.java
@@ -1,41 +1,51 @@
 package org.minu.dnd13th3backend.screentime.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.minu.dnd13th3backend.screentime.entity.ScreenTime;
-
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 @Builder
 public class ScreenTimeGetDailyResponse {
-
-    private List<ScreenTimeData> screenTimes;
+    private List<DailyScreenTime> screenTimes;
 
     public static ScreenTimeGetDailyResponse from(ScreenTime screenTime) {
         if (screenTime == null) {
-            return ScreenTimeGetDailyResponse.builder()
-                    .screenTimes(List.of())
-                    .build();
+            return ScreenTimeGetDailyResponse.builder().screenTimes(List.of()).build();
         }
-        List<ScreenTimeData> data = List.of(new ScreenTimeData(screenTime));
         return ScreenTimeGetDailyResponse.builder()
-                .screenTimes(data)
+                .screenTimes(List.of(new DailyScreenTime(screenTime)))
                 .build();
     }
 
     @Getter
-    @AllArgsConstructor
-    public static class ScreenTimeData {
+    public static class DailyScreenTime {
         private LocalDate date;
-        private int screentimeMinutes;
+        private int totalMinutes;
+        private AppTimeDetails appTimes;
 
-        public ScreenTimeData(ScreenTime screenTime) {
+        public DailyScreenTime(ScreenTime screenTime) {
             this.date = screenTime.getDate();
-            this.screentimeMinutes = screenTime.getScreentimeMinutes();
+            this.totalMinutes = screenTime.getInstagramMinutes() + screenTime.getYoutubeMinutes() +
+                    screenTime.getKakaotalkMinutes() + screenTime.getChromeMinutes();
+            this.appTimes = new AppTimeDetails(screenTime);
+        }
+    }
+
+    @Getter
+    public static class AppTimeDetails {
+        private int instagram;
+        private int youtube;
+        private int kakaotalk;
+        private int chrome;
+
+        public AppTimeDetails(ScreenTime screenTime) {
+            this.instagram = screenTime.getInstagramMinutes();
+            this.youtube = screenTime.getYoutubeMinutes();
+            this.kakaotalk = screenTime.getKakaotalkMinutes();
+            this.chrome = screenTime.getChromeMinutes();
         }
     }
 }
