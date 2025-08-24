df33e66c1e093298e6aeb3b8f17939a5e282e17e
znhxxn <ghoon7777@naver.com>
2025-08-24 20:39:47 +0900

    fix: 챌린지 코드 생성 path 수정

--- src/main/java/org/minu/dnd13th3backend/challenge/repository/ChallengeParticipantRepository.java
+++ src/main/java/org/minu/dnd13th3backend/challenge/repository/ChallengeParticipantRepository.java
@@ -2,7 +2,6 @@ package org.minu.dnd13th3backend.challenge.repository;
 
 import org.minu.dnd13th3backend.challenge.entity.Challenge;
 import org.minu.dnd13th3backend.challenge.entity.ChallengeParticipant;
-import org.minu.dnd13th3backend.challenge.type.ChallengeType;
 import org.minu.dnd13th3backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,12 +13,12 @@ import java.util.Optional;
 @Repository
 public interface ChallengeParticipantRepository extends JpaRepository<ChallengeParticipant, Long> {
 
-    Optional<ChallengeParticipant> findFirstByUserAndChallenge_TypeAndChallenge_StartDateLessThanEqualAndChallenge_EndDateGreaterThanEqualOrderByChallenge_CreatedAtDesc(
-            User user, ChallengeType type, LocalDate today1, LocalDate today2
+    List<ChallengeParticipant> findByUserAndChallenge_StartDateLessThanEqualAndChallenge_EndDateGreaterThanEqualOrderByChallenge_CreatedAtDesc(
+            User user, LocalDate today1, LocalDate today2
     );
 
-    Optional<ChallengeParticipant> findFirstByUserAndChallenge_TypeAndChallenge_StartDateAndChallenge_EndDateOrderByChallenge_CreatedAtDesc(
-            User user, ChallengeType type, LocalDate startDate, LocalDate endDate
+    List<ChallengeParticipant> findByUserAndChallenge_StartDateAndChallenge_EndDateOrderByChallenge_CreatedAtDesc(
+            User user, LocalDate startDate, LocalDate endDate
     );
 
     List<ChallengeParticipant> findByChallenge_Id(Long challengeId);
