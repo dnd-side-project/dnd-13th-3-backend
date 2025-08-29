c52eebf56bd7ac30bafd789af7313be108ec67ef
znhxxn <ghoon7777@naver.com>
2025-08-29 14:52:28 +0900

    fix: CORS 설정 수정

--- src/main/java/org/minu/dnd13th3backend/common/config/CorsConfig.java
+++ src/main/java/org/minu/dnd13th3backend/common/config/CorsConfig.java
@@ -6,20 +6,29 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("http://localhost:*");
-        configuration.addAllowedOriginPattern("http://127.0.0.1:*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://dnd-13th-3-frontend.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
         configuration.setAllowCredentials(true);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
